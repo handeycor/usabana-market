422f9a11d82b8c7e57f08c994b43875107a730a2
Harver Cordero <harvercordero@gmail.com>
2025-10-15 14:42:12 -0500

    feat: Implementación de Endpoint que procesa las compras.

---
+++ src/main/java/com/usabana/market/domain/repository/PurchaseRepository.java
@@ -0,0 +1,12 @@
+package com.usabana.market.domain.repository;
+
+import com.usabana.market.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
